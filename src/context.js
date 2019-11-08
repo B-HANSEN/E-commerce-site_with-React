8ef4dad884e7f9a30b268001888ad295920ab4d6
B-HANSEN <b.hansen.ubiqum@gmail.com>
2019-11-08 19:20:30 +0100

    replaced context logic by setting up a forEach-loop and setting State instead of referencing

--- src/context.js
+++ src/context.js
@@ -8,9 +8,24 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
     state = { 
-        products: storeProducts,
+        products: [],
         detailProduct: detailProduct
     }
+
+    componentDidMount() {
+        this.setProducts();
+    }
+
+    setProducts = () => {
+        let tempProducts = [];
+        storeProducts.forEach(item => {
+            const singleItem = { ...item };
+            tempProducts = [ ...tempProducts, singleItem ];
+        })
+        this.setState(() => {
+            return { products: tempProducts }
+        })
+    };
 
     handleDetail = () => {
         console.log('hello from detail');
@@ -29,8 +44,6 @@ class ProductProvider extends Component {
             }}>
                { this.props.children }
            </ProductContext.Provider>
-           
-
 
         );
     }
