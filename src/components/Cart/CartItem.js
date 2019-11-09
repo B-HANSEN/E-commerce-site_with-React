c69a11ec7ddb0d68ceff43c57c8df49be441d08e
B-HANSEN <b.hansen.ubiqum@gmail.com>
2019-11-09 19:12:13 +0100

    setup components CartColumns, CartList

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-export default class Cart extends Component {
+export default class CartItem extends Component {
     render() {
         return (
             <div>
-                <h3>from Cart</h3>
+                this is a cart item
             </div>
         );
     }
--- src/context.js
+++ src/context.js
@@ -12,9 +12,12 @@ class ProductProvider extends Component {
         // not required for product details, so referencing is sufficient - state would not be changed
         products: [],
         detailProduct: detailProduct,
-        cart: [],
+        cart: storeProducts,
         modalOpen: false,
-        modalProduct: detailProduct
+        modalProduct: detailProduct,
+        cartSubTotal: 0,
+        cartTax: 0,
+        cartTotal: 0
     }
 
     componentDidMount() {
@@ -70,15 +73,35 @@ class ProductProvider extends Component {
         });
     };
 
+    increment = id => {
+        console.log('this is increment method');  
+    };
+
+    decrement = id => {
+        console.log('this is decrement method'); 
+    };
+
+    removeItem = id => {
+        console.log('item removed');        
+    }
+
+    clearCart = () => {
+        console.log('cart was cleared');
+    }
+ 
 
     render() {
         return (
            <ProductContext.Provider value={{ 
-               ...this.state,
-               handleDetail: this.handleDetail,
-               addToCart: this.addToCart,
-               openModal: this.openModal,
-               closeModal: this.closeModal
+                ...this.state,
+                handleDetail: this.handleDetail,
+                addToCart: this.addToCart,
+                openModal: this.openModal,
+                closeModal: this.closeModal,
+                increment: this.increment,
+                decrement: this.decrement,
+                removeItem: this.removeItem,
+                clearCart: this.clearCart
             }}>
                { this.props.children }
            </ProductContext.Provider>
