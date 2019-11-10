1cc33a14a64c10c661d48fcd06df48ccc5146c09
B-HANSEN <b.hansen.ubiqum@gmail.com>
2019-11-10 17:16:42 +0100

    setup default component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -17,7 +17,7 @@ export default class Cart extends Component {
                         if(cart.length > 0) {
                             return (
                                 <React.Fragment>
-                                    <Title name="your" title="cart" />
+                                    <Title name="your" title=" cart" />
                                     <CartColumns />
 
                                     {/* passing down all context */}
